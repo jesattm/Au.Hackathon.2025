32f2619268bb0fbc07374688206afbefcfc463e8
Jesper Nørgaard Attermann <au578352@uni.au.dk>
2025-06-20 13:31:57 +0200

    add percentage

--- Services/StorageService.cs
+++ Services/StorageService.cs
@@ -77,5 +77,51 @@ namespace AuHackathon2025.Services
                     g => g.Sum(s => s.StorageGB)
                 );
         }
+
+        public async Task<Dictionary<string, decimal>> GetDepartmentStorageChangePercentageAsync(int days = 30)
+        {
+            // Get all storage records within the time period
+            var cutoffDate = DateTime.Now.AddDays(-days);
+            var allRecords = await _context.StorageUsages
+                .Include(s => s.Department)
+                .Where(s => s.RecordDate >= cutoffDate)
+                .OrderBy(s => s.RecordDate)
+                .ToListAsync();
+            
+            // Group by department and calculate percentage change
+            var changes = new Dictionary<string, decimal>();
+            
+            // Group records by department
+            var departmentGroups = allRecords.GroupBy(s => s.Department.Name);
+            
+            foreach (var group in departmentGroups)
+            {
+                var departmentName = group.Key;
+                var recordsByDate = group.OrderBy(s => s.RecordDate).ToList();
+                
+                // Need at least two records to calculate change
+                if (recordsByDate.Count >= 2)
+                {
+                    var oldestRecord = recordsByDate.First();
+                    var newestRecord = recordsByDate.Last();
+                    
+                    // Calculate percentage change
+                    decimal percentageChange = 0;
+                    if (oldestRecord.StorageGB > 0)
+                    {
+                        percentageChange = ((newestRecord.StorageGB - oldestRecord.StorageGB) / oldestRecord.StorageGB) * 100;
+                    }
+                    
+                    changes[departmentName] = percentageChange;
+                }
+                else if (recordsByDate.Count == 1)
+                {
+                    // If there's only one record, we can't calculate a change, so we'll show 0%
+                    changes[departmentName] = 0;
+                }
+            }
+            
+            return changes;
+        }
     }
 }
